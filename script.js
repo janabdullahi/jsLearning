9851f38cef0156b741cc7dd5919933b15fb92b02
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-01 20:24:59 +0430

    & is not valid for naming

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ console.log("Name : ", first_name)
 
 // let 3years = 3;
 // the variables con not contain numbers at begining. It's syntax error
+
+//  let and&and = 5;
+// let function = 9;
+//  the & operator and reserved keywords can not be at variable naming. it's syntx error
