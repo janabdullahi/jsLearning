664b4c7c775d54ad5df4bebcdfab45aa2fec2fc4
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-01 20:20:57 +0430

    variables does not contains number at beginig

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ if (text === 'jsLearning'){
 
 let first_name = 'abdullahi'
 console.log("Name : ", first_name)
+
+// let 3years = 3;
+// the variables con not contain numbers at begining. It's syntax error
