0f2f65a7af01e8ca50becbc613e74db2c3afdb7a
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-01 21:33:16 +0430

    let and const variable declaration

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ console.log("Name : ", first_name)
 //  let and&and = 5;
 // let function = 9;
 //  the & operator and reserved keywords can not be at variable naming. it's syntx error
+
+
+// let is changable
+let age = 22;
+age = 24;
+console.log(age)
+
+// const is not changable
+const birthday = 2000;
+console.log(birthday)
