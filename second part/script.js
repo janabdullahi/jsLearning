516eac346a7b75cc8a7519af8f9967a7fb107c85
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-02 23:48:04 +0430

    add function without parameter and with parametr

--- second part/script.js
+++ second part/script.js
@@ -8,3 +8,18 @@
 //     console.log(tree)
 // }
 //  its showing the error nad error line
+
+function logger(){
+    console.log("This is simple function without argument parameters")
+}
+
+logger();
+
+
+function para(apple, banana)
+{
+    return (`I have ${apple} apples and ${banana} bananas`)
+}
+
+const pa = para(4,4)
+console.log(pa)
