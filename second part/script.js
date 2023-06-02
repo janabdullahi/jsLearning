aa1857535343e22d04b52f1fbcdab8e31b6750f2
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-02 23:34:25 +0430

    add strict for finding errors

--- second part/script.js
+++ second part/script.js
@@ -1,3 +1,10 @@
 // strict : is used to write secure javascript code
 'use strict';
 // it should be the very first statement otherwise its not working except comment
+
+// let tree = 9;
+// if (tree <=9){
+//     tre++;
+//     console.log(tree)
+// }
+//  its showing the error nad error line
