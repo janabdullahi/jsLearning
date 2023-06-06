17de44dcf524c36a8239b85fcc83784b3dbb2bd3
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-06 22:30:49 +0430

    continue if the an array not contains numbers and change temp[i] to current Temp

--- third part/script.js
+++ third part/script.js
@@ -8,8 +8,10 @@ const funcMax = function (temp)
     let max = temp[0]
     let min = temp[0]
     for (let i = 0; i < temp.length; i++) {
-        if(temp[i]>max) max = temp[i]
-        if(temp[i]<min) min = temp[i]
+        let currTmp = temp[i]
+        if(currTmp == 'error') continue
+        if(currTmp>max) max = currTmp
+        if(currTmp<min) min = currTmp
     }
     console.log("maxx : ", max)
     console.log("minn : ", min)
