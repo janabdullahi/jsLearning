acd6caeff04b5bde6e000e39fd1f4cd619811ff2
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-09 22:22:06 +0430

    change the messages

--- third part/script.js
+++ third part/script.js
@@ -1,44 +1,44 @@
 'use strict';
-const temprature1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
-const temprature2 = [3, 4, 12, -18, 22, 91, 2, 4, 7, 14, 9, 5]
-const merge = temprature1.concat(temprature2)
+// const temprature1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
+// const temprature2 = [3, 4, 12, -18, 22, 91, 2, 4, 7, 14, 9, 5]
+// const merge = temprature1.concat(temprature2)
 
-// temprature amplitude = max value  - min value
-
-
-const funcMax = function (temp) {
-    let max = temp[0]
-    let min = temp[0]
-    for (let i = 0; i < temp.length; i++) {
-        let currTmp = temp[i]
-        if (currTmp == 'error') continue
-        if (currTmp > max) max = currTmp
-        if (currTmp < min) min = currTmp
-    }
-    console.log("maxx : ", max)
-    console.log("minn : ", min)
-    return max - min
-}
-const ampl = funcMax(merge)
-console.log("the amplitude is ", ampl)
+// // temprature amplitude = max value  - min value
 
 
+// const funcMax = function (temp) {
+//     let max = temp[0]
+//     let min = temp[0]
+//     for (let i = 0; i < temp.length; i++) {
+//         let currTmp = temp[i]
+//         if (currTmp == 'error') continue
+//         if (currTmp > max) max = currTmp
+//         if (currTmp < min) min = currTmp
+//     }
+//     console.log("maxx : ", max)
+//     console.log("minn : ", min)
+//     return max - min
+// }
+// const ampl = funcMax(merge)
+// console.log("the amplitude is ", ampl)
 
-const measurement = function () {
-    const obj = {
-        type: 'celius',
-        value: prompt("value: ") // propmt always return string
-    }
-    const kelvin = Number(obj.value) + 273 // that's why we need to convert it to number first and then add it.
-    return kelvin
-}
 
-console.log(measurement())
+
+// const measurement = function () {
+//     const obj = {
+//         type: 'celius',
+//         value: prompt("value: ") // propmt always return string
+//     }
+//     const kelvin = Number(obj.value) + 273 // that's why we need to convert it to number first and then add it.
+//     return kelvin
+// }
+
+// console.log(measurement())
 
 // DOM : document object model is for accessing the HTML elements and manipulating them
 // console.log(document.querySelector(".paragraph"));
 // console.log(document.querySelector(".paragraph").textContent);
-console.log(document.querySelector(".paragraph").textContent = 'Try To Find The Correct Number');
+console.log(document.querySelector(".paragraph").textContent = 'Try To Find The Correct Number between 1 and 20');
 
 // console.log(document.querySelector(".guess").textContent);
 // console.log(document.querySelector(".paragraph").value = '56');
@@ -59,7 +59,7 @@ document.querySelector(".click").addEventListener('click', function () {
     }
     else if (guess > number) {
         if (score > 0) {
-            console.log(document.querySelector(".paragraph").textContent = ' Too High')
+            console.log(document.querySelector(".paragraph").textContent = ' Too High the number is between 1 and 20')
             score--;
             document.querySelector(".score").textContent = "Score : " + score
         }
@@ -70,7 +70,7 @@ document.querySelector(".click").addEventListener('click', function () {
     }
     else if (guess < number) {
         if (score > 0) {
-            console.log(document.querySelector(".paragraph").textContent = ' Too Low')
+            console.log(document.querySelector(".paragraph").textContent = ' Too Low the number is between 1 and 20')
             score--;
             document.querySelector(".score").textContent = "Score : " + score
         }
