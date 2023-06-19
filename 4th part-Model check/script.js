4de588c3f2fc9a32cbad794bfcab8ab3a52c7860
abdullahi <hamedjan.abdullahi2017@gmail.com>
2023-06-19 21:58:35 +0430

    add hidden title by clicking the buttons

---
+++ 4th part-Model check/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector(".model-view")
+let paragraph = document.querySelector(".paragraph");
+let title = document.querySelector(".title");
+
+
+
+document.addEventListener("click", () => {
+    paragraph.classList.remove("hidden")
+    title.classList.remove("hidden")
+})
