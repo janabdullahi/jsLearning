ae06079f004505466d37b17b20ae261bad900857
abdullahi <jan.ab1517@gmail.com>
2023-11-05 09:17:43 +0000

    adding scores to the first player

--- pig-game/script.js
+++ pig-game/script.js
@@ -7,12 +7,23 @@ const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const btnRoll = document.querySelector('.btn--roll');
 
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+
 diceEl.classList.add('hidden');
+let currentScore = 0;
 
 btnRoll.addEventListener('click', function(){
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
+
+    if(dice !==1){
+        currentScore += dice;
+        current0El.textContent = currentScore
+    }else{
+
+    }
     
 })
