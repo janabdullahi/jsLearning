95dc6e9e89b4d04d30dad188fd27baf1da22e0b3
abdullahi <jan.ab1517@gmail.com>
2023-11-09 08:27:30 +0000

    we can add function calling before declartion only in simple function

---
+++ important points/script.js
@@ -0,0 +1,17 @@
+' use strict '
+
+console.log(adding1(2,3));
+// console.log(adding2(2,3));
+// console.log(adding3(2,3));
+
+
+function adding1(x,y){
+    return x + y;
+}
+
+
+const adding2 = function(x,y){
+    return x + y;
+}
+
+const adding3 = (x,y) => x + y;
